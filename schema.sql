af1f9b95b5f0235b5ea12a08a45a533f7eb7856d
Hamza M.Sadeeq <hamza97anh@gmail.com>
2021-06-14 23:15:03 -0500

    added retirement_info.csv and updated schema.sql

--- schema.sql
+++ schema.sql
@@ -5,6 +5,18 @@ CREATE TABLE departments (
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+select * from departments;
+-- Creating dept_emp chart
+create table dept_emp (
+	emp_no int not null,
+	dept_no varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+select * from dept_emp;
 -- Creating an employee table
 CREATE TABLE employees (
 	 emp_no INT NOT NULL,
@@ -15,16 +27,18 @@ CREATE TABLE employees (
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+select * from employees;
 -- Create Table for dept_manager with Foreign Key
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+CREATE TABLE managers (
+	dept_no VARCHAR NOT null,
+	emp_no INT NOT null,
+	from_date DATE NOT null,
+	to_date DATE NOT null,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
+select * from managers;
 -- Creating salaries tables chart
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -34,24 +48,17 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
--- Creating dept_emp chart
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
-	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
-  	PRIMARY KEY (emp_no, dept_no)
-);
+select * from salaries;
 -- Creating titles chart
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title INT NOT NULL,
-	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
 );
+select * from titles;
 
-SELECT * FROM departments; 
+
+
